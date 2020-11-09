857f1ecf670196a3638d50ceeabaae12eb618888
sara <saralundkvist86@gmail.com>
2020-11-09 13:35:54 +0100

    adds route for paypal with create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "api/auth", skip: [:omniauth_callbacks]
+  mount_devise_token_auth_for 'User', at: 'api/auth', skip: [:omniauth_callbacks]
   namespace :api do
-    resources :assignments, only: [:index, :create, :show]
+    resources :assignments, only: %i[index create show]
     resources :users, only: :show
+    resources :paypal, only: :create
   end
 end
